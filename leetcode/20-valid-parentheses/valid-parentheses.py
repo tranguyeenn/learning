1b23415277a553e8f7e94f07c6d4a46b53c6bb4e
Trang Nguyen <tranguyeenn2007@gmail.com>
2025-11-20 14:00:32 -0500

    Time: 219 ms (5.60%) | Memory: 12.6 MB (31.16%) - LeetSync

---
+++ leetcode/20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isValid(self, s):        
+        while '()' in s or '[]' in s or '{}' in s:
+            s = s.replace('()', '')
+            s = s.replace('[]', '')
+            s = s.replace('{}', '')
+
+        return s == ''    
