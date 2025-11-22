f6cf7b17f7ba3eb83b870102b3d7699d565a7220
Trang Nguyen <tranguyeenn2007@gmail.com>
2025-11-22 01:04:57 -0500

    Time: 11 ms (62.61%) | Memory: 12.4 MB (81.30%) - LeetSync

---
+++ leetcode/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        while "AB" in s or "CD" in s:
+            if "AB" in s:
+                s = s.replace("AB", "")
+            if "CD" in s:
+                s = s.replace("CD", "")
+        
+        return len(s)
