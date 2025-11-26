062cd5dc38a0fc56c249f4822b4fb807faddd035
Trang Nguyen <tranguyeenn2007@gmail.com>
2025-11-25 22:05:50 -0500

    im not failing 1301

--- concept_review/list.py
+++
@@ -1,5 +0,0 @@
-nested_list = [[10, 12], [20, 20], [30, 50]]
-
-new_list = (row[0] * row[1] for row in nested_list)
-
-print(list(new_list))
---
+++ concept_review/variables_expression/formatting.py
@@ -0,0 +1,14 @@
+import math #importing math modules
+
+#creating variables
+first_name = "Trang"
+last_name = "Nguyen"
+
+#printing variables
+print(first_name) 
+print(last_name) 
+
+#f-string formatting 
+#:.#f decimal formatting
+print(f"{(7.0/3.0):.2f}")
+print(f"{math.pi:.10f}")
