c6a4a27d8a88de2fa4ac55cf849db15201503ca6
Trang Nguyen <tranguyeenn2007@gmail.com>
2025-11-29 12:19:59 -0500

    week 2 finish

---
+++ concept_review/1301_review/representing_text.py
@@ -0,0 +1,9 @@
+print(f"Numerical encoding of the character: {ord(" ")}")
+print(f"Encoded value's character equivalent: {chr(52)}")
+
+##COMMON ESCAPE SEQUENCES
+print("\\learning\\1301_review\\") ##Backslash
+print('His name is: \'Kyline Lee\'') ##Single quote
+print("Her name is: \"Nora Andersen\"") ##Double quote
+print("My name is \nTrang Nguyen") ##Newline
+print("11-29-25 \n\t1. Calc Review \n\t2. Vscode Zybook") ##Tab Indent
