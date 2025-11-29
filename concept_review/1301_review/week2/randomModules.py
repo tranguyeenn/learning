9a99d47fea0ab0059b039c15018634e376252802
Trang Nguyen <tranguyeenn2007@gmail.com>
2025-11-29 12:07:09 -0500

    random modules

---
+++ concept_review/1301_review/week2/randomModules.py
@@ -0,0 +1,6 @@
+import random #importing random modules
+
+print(f"Generates a random floating number from 0 to 1 exclusive: {random.random()}")
+print(f"Generates a random integers with n possible values exclusive: {random.randrange(1, 20)}")
+print(f"Geneartes a random integers with n possible values: {random.randint(0, 20)}")
+
