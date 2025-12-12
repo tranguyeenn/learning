eaff560108a0a739d559454d1c09bcf7c7ba7fa1
Trang Nguyen <tranguyeenn2007@gmail.com>
2025-12-11 21:14:05 -0500

    Time: 7 ms (97.00%) | Memory: 25.8 MB (71.38%) - LeetSync

--- leetcode/217-contains-duplicate/contains-duplicate.py
+++ leetcode/217-contains-duplicate/contains-duplicate.py
@@ -4,9 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        noDup = set(nums)
-
-        if len(noDup) == len(nums):
-            return False
-        else:
-            return True
+        return len(set(nums)) != len(nums)
